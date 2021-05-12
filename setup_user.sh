2c5b9feb93ac3d2064820a1dd239bdf9735d4506
Patricia Aas <psmaas@gmail.com>
2021-05-12 15:22:18 +0200

    Update setup_user.sh

--- setup_user.sh
+++ setup_user.sh
@@ -5,7 +5,7 @@ set -e
 #  bash <(curl -s https://raw.githubusercontent.com/patricia-gallardo/insecure-vm-setup/main/setup_user.sh)
 
 apt update
-apt upgrade
+apt -y upgrade
 
 useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' $USER_PASSWORD) training
 usermod -a -G sudo training
