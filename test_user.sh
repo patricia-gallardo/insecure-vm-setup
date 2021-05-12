48b5d0943732e54f30e610bd2f46eae7737003d4
Patricia Aas <psmaas@gmail.com>
2021-05-12 18:16:35 +0200

    Update test_user.sh

--- test_user.sh
+++ test_user.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 set -e
+
+#  export IP_ADDRESS=<ip>
+#  export USER_PASSWORD=<password>
+#  bash <(curl -s https://raw.githubusercontent.com/patricia-gallardo/insecure-vm-setup/main/test_user.sh)
+
+if [[ -z "$IP_ADDRESS" ]]; then
+    echo "Must provide IP_ADDRESS in environment" 1>&2
+    exit 1
+fi
+
+if [[ -z "$USER_PASSWORD" ]]; then
+    echo "Must provide USER_PASSWORD in environment" 1>&2
+    exit 1
+fi
+
+sshpass -p $USER_PASSWORD ssh training@$IP_ADDRESS "echo $USER_PASSWORD | sudo -S apt update"
