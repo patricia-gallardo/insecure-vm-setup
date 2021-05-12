4122f42817daffd9317b1290ebabc4245838a6c2
Patricia Aas <psmaas@gmail.com>
2021-05-12 14:51:49 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
 
+#  export USER_PASSWORD=<password>
+#  bash <(curl -s https://raw.githubusercontent.com/patricia-gallardo/insecure-vm-setup/main/setup.sh)
+
 apt update
 apt upgrade
+
+adduser -p $USER_PASSWORD training
+usermod -a -G sudo training
+sudo reboot
