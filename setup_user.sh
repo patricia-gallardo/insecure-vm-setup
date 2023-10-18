dc9be2da58e30c0261f43065f7c64f36c599e225
Patricia Aas <psmaas@gmail.com>
2023-10-18 18:35:54 +0200

    Update setup_user.sh
    Add output

--- setup_user.sh
+++ setup_user.sh
@@ -9,10 +9,14 @@ if [[ -z "$USER_PASSWORD" ]]; then
     exit 1
 fi
 
+echo "Update Droplet" 1>&2
 apt update
 apt -y upgrade
 
+echo "Add user" 1>&2
 useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' $USER_PASSWORD) training
 usermod -a -G sudo training
 usermod --shell /bin/bash training
+
+echo "Reboot" 1>&2
 sudo reboot
